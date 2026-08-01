30acceb632277c4eaf535037f7bd3a12986d19a7
adwaidh-user <adwaith3105@gmail.com>
2026-08-01 19:31:00 +0530

    multi-clients
    multiple clients can connect to same server instance, but no concurrency

--- phase_0/tcp_server.c
+++ phase_0/tcp_server.c
@@ -2,7 +2,6 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -12,63 +11,67 @@
 #define BUFF_SIZE 10000
 #define MAX_ACCEPT_BACKLOG 5
 
-void strrev(char* str) {
-    for (int l = 0, r = strlen(str) - 2; l < r; l++, r--) {
-        char temp = str[l];
-        str[l] = str[r];
-        str[r] = temp;
-    }
+void strrev(char *str) {
+	for (int l = 0, r = strlen(str) - 2; l < r; l++, r--) {
+		char temp = str[l];
+		str[l] = str[r];
+		str[r] = temp;
+	}
 }
 
 int main() {
-    int listen_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int listen_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Setting socket to reuse addr in case restarted while in TIME_WAIT
-    int enable = 1;
-    setsockopt(listen_sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+	// Setting socket to reuse addr in case restarted while in TIME_WAIT
+	const int enable = 1;
+	setsockopt(listen_sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
 
-    // Setting the server addr
-    struct sockaddr_in server_addr = {
-        .sin_family = AF_INET,
-        .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port = htons(PORT)
-    };
+	// Setting the server addr
+	struct sockaddr_in server_addr = {.sin_family = AF_INET,
+									  .sin_addr.s_addr = htonl(INADDR_ANY),
+									  .sin_port = htons(PORT)};
 
-    // binding the server
-    int bind_output = bind(listen_sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
+	// binding the server
+	int bind_output = bind(listen_sock_fd, (struct sockaddr *)&server_addr,
+						   sizeof(struct sockaddr_in));
 
-    //Start listening
-    listen(listen_sock_fd, MAX_ACCEPT_BACKLOG);
-    printf("[INFO] Server listening on port %d\n", PORT);
+	// Start listening
+	listen(listen_sock_fd, MAX_ACCEPT_BACKLOG);
+	printf("[INFO] Server listening on port %d\n", PORT);
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len;
+	struct sockaddr_in client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
 
-    int conn_sock_fd = accept(listen_sock_fd, ( struct sockaddr * )&client_addr, &client_addr_len);
-    printf("[INFO] Client connected to server\n");
+	while (1) {
+		int conn_sock_fd = accept(
+			listen_sock_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+		printf("[INFO] Client connected to server\n");
 
-    while (1) {
-        char buf[BUFF_SIZE]; 
-        memset(buf, 0, BUFF_SIZE);
+		while (1) {
+			char buf[BUFF_SIZE];
+			memset(buf, 0, BUFF_SIZE);
 
-        ssize_t read_n = recv(conn_sock_fd, buf, sizeof(buf), 0);
+			ssize_t read_n = recv(conn_sock_fd, buf, sizeof(buf), 0);
 
-        if (read_n < 0) {
-            printf("[INFO] Error occured. Closing server\n");
-            close(conn_sock_fd);
-            exit(1);
-        } else if (read_n == 0) {
-            printf("[INFO] Client disconnected. Closing server\n");
-            close(conn_sock_fd);
-            exit(1);
-        }
+			if (read_n < 0) {
+				printf("[INFO] Error occured. Closing server\n");
+				close(conn_sock_fd);
+                break;
+			} else if (read_n == 0) {
+				printf("[INFO] Client disconnected. Closing server\n");
+				close(conn_sock_fd);
+                break;
+			}
 
-        printf("[Client message] %s", buf);
+			printf("[CLIENT MESSAGE] %s", buf);
 
-        strrev(buf);
+			strrev(buf);
 
-        send(conn_sock_fd, buf, read_n, 0);
-    }
+			send(conn_sock_fd, buf, read_n, 0);
+		}
+	}
 
-    return 0;
+    close(listen_sock_fd);
+
+	return 0;
 }
